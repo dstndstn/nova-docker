b61a1540053e9f4259dd0b867fe7e01381a15714
gnthibault <gnthibault@gmail.com>
2018-01-18 13:21:40 +0100

    Now process_submission is called with the right option

--- process_submission.sh
+++ process_submission.sh
@@ -2,6 +2,6 @@
 
 cd /src/astrometry/net
 export PYTHONPATH=${PYTHONPATH}:.
-for ((;;)); do python process_submissions.py -s 4 -j 16 > dj.log 2>&1; sleep 1; done
+for ((;;)); do python process_submissions.py --solve-locally=$(pwd)/solvescript.sh  -s 4 -j 16 > dj.log 2>&1; sleep 1; done
 
 
